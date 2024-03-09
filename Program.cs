bda2aa20dd0dac0ea3dabdda71c0c81c88f9aa47
unknown <nadezhda.tarasova8@ya.ru>
2024-03-09 15:11:48 +0300

    add task 3

--- Program.cs
+++ Program.cs
@@ -23,20 +23,36 @@
 // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 
-int Ackermann(int m, int n)
-{
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    else if (m > 0 && n == 0)
-    {
-        return Ackermann(m - 1, 1);
-    }
-    else
-    {
-        return Ackermann(m - 1, Ackermann(m, n - 1));
-    }
-}
+// int Ackermann(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (m > 0 && n == 0)
+//     {
+//         return Ackermann(m - 1, 1);
+//     }
+//     else
+//     {
+//         return Ackermann(m - 1, Ackermann(m, n - 1));
+//     }
+// }
 
-Console.WriteLine($"Результат: {Ackermann (3,5)}");
+// Console.WriteLine($"Результат: {Ackermann (3,5)}");
+
+
+// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintArray (int [] array, int index)
+{
+    if (index < 0)
+    {
+       return;
+    }
+    Console.Write ($"{array[index]} ");
+    PrintArray(array, index-1);
+}
+int [] arr = {1,2,5,0,10,34};
+PrintArray(arr, arr.Length-1);
