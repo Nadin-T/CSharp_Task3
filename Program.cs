460cced3ae1445414b94ebd6227c712b59e006ed
unknown <nadezhda.tarasova8@ya.ru>
2024-03-09 14:44:39 +0300

    add task 2

--- Program.cs
+++ Program.cs
@@ -2,19 +2,41 @@
 // все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
 
-void ShowNumbers (int M, int N)
+// void ShowNumbers (int M, int N)
+// {
+//     if (M > N)
+//     {
+//         return;
+//     }
+//     Console.Write($"{M} ");
+//     ShowNumbers (M+1, N);
+// }
+
+// Console.WriteLine ("Введите число M:");
+// int M = Convert.ToInt32 (Console.ReadLine ());
+// Console.WriteLine ("Введите число N:");
+// int N = Convert.ToInt32 (Console.ReadLine ());
+
+// ShowNumbers (M,N);
+
+
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int Ackermann(int m, int n)
 {
-    if (M > N)
+    if (m == 0)
     {
-        return;
+        return n + 1;
     }
-    Console.Write($"{M} ");
-    ShowNumbers (M+1, N);
+    else if (m > 0 && n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
 }
 
-Console.WriteLine ("Введите число M:");
-int M = Convert.ToInt32 (Console.ReadLine ());
-Console.WriteLine ("Введите число N:");
-int N = Convert.ToInt32 (Console.ReadLine ());
-
-ShowNumbers (M,N);
+Console.WriteLine($"Результат: {Ackermann (3,5)}");
